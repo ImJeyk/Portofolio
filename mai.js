a120fe974452ff7d2d662226be5a0609fa3ef26c
ImJeyk <idyk395@gmail.com>
2024-05-20 09:17:52 +0700

    my project

---
+++ mai.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", {
+  strings: ["Mahasiswa", "Komputer Informatika", "TRPL"],
+  typeSpeed: 20,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+alert("Selamat datang di WEB saya");
